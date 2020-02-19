97efd8f5928bda9a9a154800e30162d52e20925a
Alberto Cruz <albertocruzmendoza@outlook.com>
2020-02-18 20:49:04 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,39 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
-})
-export class AppComponent {
-  title = 'cash';
+        })
+        export class AppComponent {
+        title = 'cash';
+
+        Saldo = 0;
+        Transacciones = [
+        ];
+
+        dep(cant: HTMLInputElement) {
+        if (cant.value === '') {
+          alert('Tiene que ingresar una cantidad');
+        } else {
+        if (cant.valueAsNumber >= 0) {
+          this.Saldo = this.Saldo + cant.valueAsNumber;
+          this.Transacciones.unshift(cant.valueAsNumber);
+        } else {
+          alert('No puede ingresar valores negativos');
+        }
+      }
+
+    }
+
+   ret(cant: HTMLInputElement) {
+     if (cant.value === '') {
+        alert('Tiene que ingresar una cantidad');
+     } else {
+        if (cant.valueAsNumber >= 0) {
+       this.Saldo = this.Saldo - cant.valueAsNumber;
+       this.Transacciones.unshift(-cant.valueAsNumber);
+      } else {
+       alert('No puede ingresar valores negativos');
+      }
+    }
+  }
+
 }
